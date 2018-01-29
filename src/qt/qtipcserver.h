6edab1e3be3dda78d44b70dc380b181eec535a91
contanti <contanti.super@gmail.com>
2018-01-29 09:55:45 +0700

    first commit

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Contanti-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "ContantiURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
